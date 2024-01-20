85d70444b4418b840b04544ca55374b68c6b69c4
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-01-20 20:09:19 +0000

    Edit Home page

--- view/Home.php
+++ view/Home.php
@@ -1,8 +1,19 @@
-<?php
-
-?>
 <main>
-    <h1>Home</h1>
-    <p>Welcome to the music shop!</p>
-    <p>Shop all kinds of whimsical musical equipment.</p>
+    <h1>Welcome to Clef</h1>
+    <p>Unlock your music potential with us!</p>
+    <br>
+    <p>Here at Clef you can shop all kinds of whimsical musical equipment and everything you need for your musical journey.</p>
+    <p>You can choose from:</p>
+    <ul>
+        <li>Musical Instruments</li>
+        <li>Accessories</li>
+        <li>Audio Interfaces</li>
+        <li>Headphones</li>
+        <li>Studio monitors</li>
+        <li>Software</li>
+    </ul>
+    <p>and more!</p>
+    <br>
+    <p>So what are you waiting for? <a href="?p=register">Create an account</a> and start shopping.</p>
+    <p>Hell, we'll even throw in a 25% off your first order.</p>
 </main>
